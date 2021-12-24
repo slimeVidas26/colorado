d207fd8bc5ddf6b47269c9adcee4e5c09b21c8e6
SlimeVidas <dahan.isaac@gmail.com>
2021-12-24 15:18:27 +0200

    amplify init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "colorado": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
